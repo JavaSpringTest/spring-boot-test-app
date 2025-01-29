fe8e79f2ed038317aea073e3cc1d58d084a3463d
Luis Angel <luis.flores@durango.gob.mx>
2025-01-29 15:23:01 -0600

    Creacion de testing para JPA

--- src/main/java/com/angelfg/app/repositories/CuentaRepository.java
+++ src/main/java/com/angelfg/app/repositories/CuentaRepository.java
@@ -2,6 +2,10 @@ package com.angelfg.app.repositories;
 
 import com.angelfg.app.models.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 // import java.util.List;
 
@@ -9,4 +13,10 @@ public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
     //List<Cuenta> findAll();
     //Cuenta findById(Long id);
     //void update(Cuenta cuenta);
+
+    @Query("select c from Cuenta c where c.persona = ?1")
+    Optional<Cuenta> findByPersona(String persona);
+
+//     Optional<Cuenta> findByPersona(String persona);
+
 }
---
+++ src/test/java/com/angelfg/app/IntegracionJpaTest.java
@@ -0,0 +1,59 @@
+package com.angelfg.app;
+
+import com.angelfg.app.models.Cuenta;
+import com.angelfg.app.repositories.CuentaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest // Habilita los test para el repository
+public class IntegracionJpaTest {
+    
+    @Autowired
+    private CuentaRepository cuentaRepository;
+
+    @Test
+    void testFindById() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findById(1L);
+        assertTrue(cuenta.isPresent());
+        assertEquals("Luis", cuenta.orElseThrow().getPersona());
+    }
+
+    @Test
+    void testFindByPersona() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findByPersona("Luis");
+        assertTrue(cuenta.isPresent());
+        assertEquals("Luis", cuenta.orElseThrow().getPersona());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = this.cuentaRepository.findByPersona("Rod");
+
+//        assertThrows(NoSuchElementException.class, () -> {
+//            cuenta.orElseThrow();
+//        });
+
+//        assertThrows(NoSuchElementException.class, () -> cuenta.orElseThrow());
+
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = this.cuentaRepository.findAll();
+
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+    }
+
+}
