5b0e4ad6ad9c05dfb9ca5d958d0cf662f593f2b5
Luis Angel <luis.flores@durango.gob.mx>
2025-01-30 09:55:16 -0600

    Mejorando coverage de testing 100%

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-
-
-
-INSERT INTO cuentas (persona, saldo) VALUES ('Luis', 1000);
-INSERT INTO cuentas (persona, saldo) VALUES ('Andres', 2000);
-
-INSERT INTO bancos (nombre, total_transferencias) VALUES ('El banco financiero', 0);
