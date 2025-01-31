d47954c1104239e166ad9f985527957345c63073
Luis Angel <luis.flores@durango.gob.mx>
2025-01-31 15:54:29 -0600

    Creacion de una cuenta prueba de integracion post

--- src/test/java/com/angelfg/app/controllers/CuentaControllerWebTestClientTests.java
+++ src/test/java/com/angelfg/app/controllers/CuentaControllerWebTestClientTests.java
@@ -119,6 +119,7 @@ class CuentaControllerWebTestClientTests {
                 .consumeWith(response -> {
                    Cuenta cuenta = response.getResponseBody();
 
+                   assertNotNull(cuenta);
                    assertEquals("Andres", cuenta.getPersona());
                    assertEquals("2100.00", cuenta.getSaldo().toPlainString());
 
@@ -168,6 +169,57 @@ class CuentaControllerWebTestClientTests {
                 })
                 .hasSize(2)
                 .value(hasSize(2));
+
+    }
+
+    @Test
+    @Order(6)
+    void testGuardar() {
+
+        // Given
+        Cuenta cuenta = new Cuenta(null, "Pepe", new BigDecimal("3000"));
+
+        // When
+        client.post().uri("/api/cuentas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(cuenta)
+                .exchange()
+                // Then
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(3)
+                .jsonPath("$.persona").isEqualTo("Pepe")
+                .jsonPath("$.persona").value(is("Pepe"))
+                .jsonPath("$.saldo").isEqualTo(3000);
+
+    }
+
+    @Test
+    @Order(7)
+    void testGuardar2() {
+
+        // Given
+        Cuenta cuenta = new Cuenta(null, "Mar", new BigDecimal("3500"));
+
+        // When
+        client.post().uri("/api/cuentas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(cuenta)
+                .exchange()
+                // Then
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Cuenta.class)
+                .consumeWith(response -> {
+                    Cuenta c = response.getResponseBody();
+
+                    assertNotNull(c);
+                    assertEquals(4L, c.getId());
+                    assertEquals("Mar", c.getPersona());
+                    assertEquals("3500", c.getSaldo().toPlainString());
+
+                });
 
     }
 
